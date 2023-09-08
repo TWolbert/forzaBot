526e37788a5da238c70429027112e97065754553
Teun Wolbert <teunwolbert@gmail.com>
2023-09-09 00:03:06 +0200

    four pluh

--- express.js
+++ express.js
@@ -149,8 +149,8 @@ io.on('connection', (socket) => {
 const con = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
 
